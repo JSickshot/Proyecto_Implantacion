4bf39c36336080e91de4106fd6f84b87e4c24d29
Enrique2342 <140670775+Enrique2342@users.noreply.github.com>
2023-11-30 15:05:57 -0600

    jav

--- JavaScript/materias.js
+++ JavaScript/materias.js
@@ -14,6 +14,20 @@ function cargarMaterias() {
         "Redes Local",
         "Protocolos de redes",
         "Sistemas Operativos",
+        "Lenguaje y comunacion",
+        "Ciencias Naturales",
+        "Ciencias Sociales",
+        "Historia",
+        "Filosofia",
+        "Ingles",
+        "Leyes",
+        "Quimica",
+        "Fisica",
+        "Biologia",
+        "Geografia",
+        "Tecnologia",
+        "Artes",
+        "Español",
     ];
 
     for (var i = 0; i < opcionesMaterias.length; i++) {
@@ -31,11 +45,26 @@ function cargarHorarios() {
 
     var opcionesHorarios = [
         "Lunes 8:00 AM - 10:00 AM",
+        "Lunes 10:00 AM - 12:00 PM",
+        "Lunes 12:00 PM - 2:00 PM",
+        "Lunes 2:00 PM - 4:00 PM",
         "Martes 2:00 PM - 4:00 PM",
+        "Martes 4:00 PM - 6:00 PM",
+        "Martes 6:00 PM - 8:00 PM",
+        "Martes 8:00 PM - 10:00 PM",
         "Miércoles 1:00 PM - 3:00 PM",
+        "Miércoles 3:00 PM - 5:00 PM",
+        "Miércoles 5:00 PM - 7:00 PM",
+        "Miércoles 7:00 PM - 9:00 PM",
         "Jueves 7:00 AM - 9:00 AM",
+        "Jueves 9:00 AM - 11:00 AM",
+        "Jueves 11:00 AM - 1:00 PM",
+        "Jueves 1:00 PM - 3:00 PM",
+        "Viernes 6:00 AM - 8:00 AM",
         "Viernes 9:00 AM - 12:00 PM",
-        "Sábado 8:00 AM - 10:00 AM",
+        "Viernes 1:00 PM - 3:00 PM",
+        "Viernes 4:00 PM - 6:00 PM",
+        
     ];
 
     for (var i = 0; i < opcionesHorarios.length; i++) {
