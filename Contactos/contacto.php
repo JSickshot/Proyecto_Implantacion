4073f9ed7ae61671307d692ef2b0718dd9d57b2c
Enrique2342 <140670775+Enrique2342@users.noreply.github.com>
2023-11-30 00:35:35 -0600

    colegiaturas

--- Contactos/contacto.php
+++ Contactos/contacto.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/vision.css">
+    <link rel="stylesheet" href="../css/conctato.css">
     <title>Contacto</title>
     
 </head>
@@ -12,7 +12,14 @@
 <body>
     <div class="container">
         <h1>Contacto</h1>
-        <p>Num, correo, whatsapp, facebook, instagram</p>
+        <p>Puedes contactarnos a través de los siguientes medios:</p>
+        <ul>
+            <li>Número telefónico: 5543914928</li>
+            <li>Correo electrónico: TaquitosSupremos@my.campechanos.edu.mx</li>
+            <li>WhatsApp: 333434452</li>
+            <li>Facebook: https://www.facebook.com/groups/1343483</li>
+            <li>Instagram: https://instagram.com/taquitos</li>
+        </ul>
 
         <a href="#" class="boton-favor" onclick="history.go(-1); return false;"> Regresar a nuestro portal </a>
 
